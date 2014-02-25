462a0ea9ecfdaacc4648868074af61030fe83d55
Terumi <ett1n>
2014-02-25 20:09:46 +0200

    Initial commit

---
+++ src/Ffy/Photogallery/PhotogalleryServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Ffy\Photogallery;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhotogalleryServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ffy/photogallery');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
