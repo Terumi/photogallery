39637dbe9c685aeb7c5b8c0784206935f8202667
Terumi <ett1n>
2014-03-04 17:33:28 +0200

    favorite migration

---
+++ src/migrations/2014_03_04_135541_add_favorite_to_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFavoriteToPhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('ffy_photos', function(Blueprint $table) {
+            $table->boolean('favorite')->default(0);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('ffy_photos', function($table)
+        {
+            $table->dropColumn('favorite');
+        });
+	}
+
+}
