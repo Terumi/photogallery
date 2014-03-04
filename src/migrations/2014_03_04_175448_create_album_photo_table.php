44748729a7553bc8c1dfccae85732991b80d6d11
Terumi <ett1n>
2014-03-04 20:38:08 +0200

    album_photo relation/migration

---
+++ src/migrations/2014_03_04_175448_create_album_photo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumPhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('ffy_album_photo', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('album_id')->references('id')->on('ffy_albums')->onDelete('cascade');
+            $table->integer('photo_id')->references('id')->on('ffy_photos')->onDelete('cascade');
+            $table->timestamps('');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('ffy_album_photo');
+	}
+
+}
