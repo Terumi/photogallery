f08e6d5a37aba50578b375d9250bd975d76f54e5
Terumi <ett1n>
2014-03-05 17:12:35 +0200

    albums table

--- src/migrations/2014_03_04_162423_create_albums_table_now.php
+++ src/migrations/2014_03_04_162423_create_albums_table_now.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateAlbumsTable extends Migration {
+class CreateAlbumsTableNow extends Migration {
 
 	/**
 	 * Run the migrations.
